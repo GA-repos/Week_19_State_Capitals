adb44eb227118a3d551a71912ad905484ebef71f
Paresh10 <paresh.sharma10@gmail.com>
2022-02-26 15:02:08 -0800

    Instructioon for state capitol

---
+++ app.py
@@ -0,0 +1,38 @@
+
+from capitals import states
+import random
+
+random.shuffle(states)
+
+# for state in states:
+    # print(state['name'], state['capital'])
+    # print(state['capital'])
+
+while True:
+    name = input("Enter your name: ")
+    if name.lower() == 'matt':
+        break
+
+
+
+# welcome_msg = f"Start the game now, {name}"
+
+
+
+# print(welcome_msg)
+
+# while Loop
+
+# print(states)
+
+# Input / Output program
+
+# Ask user what is the capital of the state
+    # IF correct correct += 1
+    # IF INCORRECT Then incorrect += 1 
+
+# Ask user weather they wanna play again?
+    #if yes 
+        # game()
+    # if no
+        # exit STATS
